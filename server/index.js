8ff4c0c79fc0b93646518b23f92c861ec26afe78
Mykola <kolya182@gmail.com>
2017-08-09 14:08:41 -0700

    Add skeletons for POST and GET

--- server/index.js
+++ server/index.js
@@ -7,6 +7,14 @@ let port = 3000;
 //Serve public folder at app root URL
 app.use(express.static(__dirname + '/../client/dist'));
 
+app.get('/', (req, res) => {
+  res.send('Hello');
+});
+
+app.post('/event', (req, res) => {
+
+});
+
 //Starts server and listens for request
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
