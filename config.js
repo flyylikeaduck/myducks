d08bf64e1fe6a9e13df2b4b0b25af52ecda06660
Mykola <kolya182@gmail.com>
2017-08-15 19:08:03 -0700

    Add heroku files

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 // https://help.github.com/articles/creating-a-personal-access-token-for-the-command-line/
 module.exports = {
   twilio: {
-    accountSid: '',
-    authToken: '',
+    accountSid: 'AC00b8c28af50b5bc3a89881783674b015',
+    authToken: 'aed4812fb57cf70ef699bb7c7c778236',
     testsPhones: [],
     testsPhonesMe: [],
   },
