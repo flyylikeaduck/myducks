b39d5c910b29ba0aefb99966653f0ceee812a739
Rebecca Chen <flyylikeaduck@gmail.com>
2017-08-16 14:11:13 -0700

    Update bodyParser functionality in server index

--- server/index.js
+++ server/index.js
@@ -1,20 +1,33 @@
 const express = require('express');
+const checkMysql = require('../database/checkMysql.js');
 const db = require('../database/index.js');
 const bodyParser = require('body-parser');
 const twilio = require('./helpers/twilio.js');
 
-const checkMysql = require('../database/checkMysql.js');
 
 //Create instance of express
 let app = express();
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
 let port = process.env.PORT || 3000;
-let urlencodedParser = bodyParser.urlencoded({ extended: false });
+
 //Serve public folder at app root URL
 app.use(express.static(__dirname + '/../client/dist'));
+
+
+
 app.get('/', express.static('public'));
 
-app.post('/event', urlencodedParser, (req, res) => {
+app.post('/createUser', (req, res) => {
+req.address
+  db.createUser(req.username, req.address, req.phoneNumber)
+    .then()
+})
+
+
+app.post('/event', (req, res) => {
   let lat = req.query.latitude;
   let lng = req.query.longitude;
   let eventType = req.query.eventType;
