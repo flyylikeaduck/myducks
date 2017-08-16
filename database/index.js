b62079b98d61f328be3edea431974c2ed05822ed
Rebecca Chen <flyylikeaduck@gmail.com>
2017-08-16 16:59:07 -0700

    Add createUser db helper

--- database/index.js
+++ database/index.js
@@ -1,81 +1,21 @@
-// const checkMysql = require('./checkMysql.js');
-const Sequelize = require('sequelize');
+const models = require("./models");
 
-const sequelize = new Sequelize('safety_buddies', 'root', '', {
-  host: 'localhost',
-  dialect: 'mysql',
-  port: 3306
-});
+module.exports.getUsers = () => {
 
-// creates 'Users' table
-// sequelize automatically adds id, createdAt, and updatedAt cols
-let User = sequelize.define('user', {
-  username: {
-    type: Sequelize.STRING,
-    notEmpty: true,
-    allowNull: false,
-    unique: true
-  },
-  address: {
-    type: Sequelize.STRING
-  },
-  // may need email if we use OAUTH
-  // email: {
-  //   type: Sequelize.STRING,
-  //   validate: {
-  //     isEmail: true
-  //   }
-  // },
-  phone: {
-    type: Sequelize.STRING
-  }
-});
-
-let Event = sequelize.define('event', {
-  id: {
-    autoIncrement: true,
-    type: Sequelize.INTEGER
-  },
-  description: {
-    type: Sequelize.STRING,
-    notEmpty: true,
-    allowNull: false
-  },
-  lat: {
-    type: Sequelize.DECIMAL,
-    notEmpty: true,
-    allowNull: false
-  },
-  lng: {
-    type: Sequelize.DECIMAL,
-    notEmpty: true,
-    allowNull: false
-  }
-})
-
-// adds foreign key to Event
-Event.belongsTo(User, {
-  foreignKey: {
-    name: 'user_id',
-    allowNull: false
-  }
-})
-
-
-// Sync db
-
-sequelize
-  .sync() // no need to drop Users table each time
-  .then(function() {
-    console.log('Database is working!');
-  })
-  .catch(function(err) {
-    console.log('An err occurred while updating the db:', err);
-  });
-
-
-module.exports = {
-  User,
-  Event
 }
 
+module.exports.createUser = (username, address, phone) => {
+  // add '+1 to phone'
+  return models.User.create({
+    username, address, phone
+  });
+}
+
+module.exports.createEvent = (lat, long) => {
+  return models.Event.create({
+
+  })
+}
+
+
+module.exports.filterByUser
