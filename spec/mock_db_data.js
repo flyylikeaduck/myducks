bbdaf84287ccdb284fd6178a1ab8f22a2d0aa9b3
Rebecca Chen <flyylikeaduck@gmail.com>
2017-08-16 16:59:27 -0700

    Add mock db data

--- spec/mock_db_data.js
+++ spec/mock_db_data.js
@@ -1,3 +1,32 @@
-var users = {
+const credentials = require('../config.js');
 
-}
+window.users = [
+  {
+    "id": 1,
+    "username": "kolya",
+    "location": {
+      "lat" : 37.782716,
+      "lng" :  -122.410175
+    }
+    "phoneNumber": credentials.usersDb.kolyaNum
+  },
+  {
+    "id": 2,
+    "username": "flyyduck",
+    "location": {
+      "lat" : 37.4224764,
+      "lng" : -122.0842499
+    }
+    "phoneNumber": credentials.usersDb.flyyNum
+  },
+   {
+    "id": 3,
+    "username": "blkdm0n",
+    "location": {
+      "lat" : 37.781119,
+      "lng" : -122.406306
+    }
+    "phoneNumber": credentials.usersDb.blkNum
+  },
+
+]
