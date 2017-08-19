6879ebb2bfa96e3c886537973d6f2f711aff8bbf
Mykola <kolya182@gmail.com>
2017-08-18 19:07:00 -0700

    Remove autoIncrement for id colomn in event table

--- database/models.js
+++ database/models.js
@@ -1,11 +1,11 @@
-// const checkMysql = require('./checkMysql.js');
 const Sequelize = require('sequelize');
 
-const sequelize = new Sequelize('safety_buddies', 'root', '', {
-  host: 'localhost',
-  dialect: 'mysql',
-  port: 3306
-});
+const sequelize = new Sequelize(process.env.JAWSDB_URL);
+// const sequelize = new Sequelize(process.env.DB_DATABASE, process.env.DB_USERNAME, process.env.DB_PASSWORD, {
+//   host: 'localhost',
+//   dialect: 'mysql',
+//   port: 3306
+// });
 
 // creates 'Users' table
 // sequelize automatically adds id, createdAt, and updatedAt cols
@@ -14,45 +14,11 @@ let User = sequelize.define('user', {
     type: Sequelize.STRING,
     notEmpty: true,
     allowNull: false,
-    unique: true
+    // unique: true
   },
   // postponegeocoding for now
   address: {
     type: Sequelize.STRING
-  },
-  // lat: {
-  //   type: Sequelize.DECIMAL,
-  //   notEmpty: true,
-  //   allowNull: false
-  // },
-  // lng: {
-  //   type: Sequelize.DECIMAL,
-  //   notEmpty: true,
-  //   allowNull: false
-  // }
-  // may need email if we use OAUTH
-  // email: {
-  //   type: Sequelize.STRING,
-  //   validate: {
-  //     isEmail: true
-  //   }
-  // },
-  phone: {
-    type: Sequelize.STRING,
-    notEmpty: true,
-    allowNull: false
-  }
-});
-
-let Event = sequelize.define('event', {
-  id: {
-    autoIncrement: true,
-    type: Sequelize.INTEGER
-  },
-  description: {
-    type: Sequelize.STRING,
-    notEmpty: true,
-    allowNull: false
   },
   lat: {
     type: Sequelize.DECIMAL,
@@ -63,23 +29,57 @@ let Event = sequelize.define('event', {
     type: Sequelize.DECIMAL,
     notEmpty: true,
     allowNull: false
-  }
-})
-
-// adds foreign key to Event
-Event.belongsTo(User, {
-  foreignKey: {
-    name: 'user_id',
+  },
+  // may need email if we use OAUTH
+  email: {
+    type: Sequelize.STRING,
+    validate: {
+      isEmail: true
+    }
+  },
+  phone: {
+    type: Sequelize.STRING,
+    notEmpty: true,
     allowNull: false
   }
-})
+});
+
+let Event = sequelize.define('event', {
+  description: {
+    type: Sequelize.STRING,
+    notEmpty: true,
+    allowNull: false
+  },
+  latitude: {
+    type: Sequelize.INTEGER,
+    allowNull: true,
+    defaultValue: null,
+    validate: { min: -90, max: 90 },
+  },
+  longitude: {
+    type: Sequelize.INTEGER,
+    allowNull: true,
+    defaultValue: null,
+    validate: { min: -180, max: 180 },
+  }
+});
+
+Event.belongsTo(User);
+
+// adds foreign key to Event
+// Event.belongsTo(User, {
+//   foreignKey: {
+//     name: 'user_id',
+//     allowNull: false
+//   }
+// });
 
 
 // Sync db
 
 sequelize
   .sync() // no need to drop Users table each time
-  .then(function() {
+  .then(() => {
     console.log('Database is working!');
   })
   .catch(function(err) {
